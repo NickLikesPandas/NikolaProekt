c3641e07ab18ad885e69b06020256a9a96dc1692
Nikola <nikjoo12@gmail.com>
2025-11-28 11:12:37 +0200

    modified:   database/migrations/2025_11_21_123852_create_images_table.php

--- database/migrations/2025_11_21_123852_create_images_table.php
+++ database/migrations/2025_11_21_123852_create_images_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->string('title');
             $table->string('src'); // file name
             $table->timestamps();
-    
+    //sdsadsadadadadsadsadsadasdsa
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
         });
     }
